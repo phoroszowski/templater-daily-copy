2a8893a9418d2c967dabcfb0e33fabe84b00ec1e
Peter Horoszowski <peter@boolean.tv>
2024-04-12 16:30:12 -0400

    attempt to copy with update

--- copy_from_yesterday.js
+++ copy_from_yesterday.js
@@ -1,4 +1,4 @@
-copy_from_yesteday =  async (strMatch, strExlusion) => {
+copy_from_yesteday =  async (strMatchTrigger, strExlusion) => {
     
     let currentFile = this.app.workspace.getActiveFile();
     let today = new Date(currentFile.basename+ "T12:00");
@@ -11,7 +11,7 @@ copy_from_yesteday =  async (strMatch, strExlusion) => {
     const files = this.app.vault.getMarkdownFiles();
 
     //Build our a search history 
-    let historySearchCount = 10;
+    let historySearchCount = 20;
     for(let i = 0 ; i < historySearchCount; i++)
     {
         yesterday.setDate(yesterday.getDate() - 1 );
@@ -46,25 +46,53 @@ copy_from_yesteday =  async (strMatch, strExlusion) => {
     console.log(bestMatchedFile.path);
     let text = await this.app.vault.read(bestMatchedFile);
     
+    let updatedHistory = "";
     let arrLines = text.split('\n');
     let strCopyLines = "";
     let copyOn = false;
+    let bAlteredLine = false;
     for(line of arrLines)
     {   
-        console.log(`${copyOn? "Copying": "Not Copying"}: ${line}`);
         if(copyOn && line.indexOf(strExlusion) == -1 && line.indexOf("<hr>") == -1)
-            strCopyLines += line + "\n"
-        if(line.indexOf(`${strMatch}`) > -1)
         {
-            console.log(line);
+            strCopyLines += line + "\n";
+            
+        }
+        if(copyOn && line.indexOf(strExlusion) > 0 && line.indexOf("<hr>") == -1)
+        {
+            // Altering Task to 
+            let strAlteredLine = line.replace(strExlusion, "[Completed]");
+            updatedHistory += strAlteredLine + "\n";
+            bAlteredLine = true;
+        }
+        if(line.indexOf(`${strMatchTrigger}`) > -1)
+        {
+            
             copyOn = true;
         }
         if(line.indexOf("<hr>") > -1 && copyOn == true)
         {
             copyOn = false;
         }
+        if(bAlteredLine == false)
+        {
+            updatedHistory += line + "\n";
+        }
+        bAlteredLine = false;
         
     }
+
+    console.log(`Modifiying: ${bestMatchedFile.path}`);  
+ 
+    
+    this.app.vault.process(bestMatchedFile, (data) =>{
+        console.log('$$$$$$');
+        console.log(data);
+        return updatedHistory;
+    });
+    
+    console.log(`Modifiyed: ${bestMatchedFile.path}`);
+    
 
     return(strCopyLines);   
  ;
